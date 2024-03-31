9dedae6936fa380ce5f18656e0bb4e2b7c30111d
Xavier Dambreville <xavierdambreville@gmail.com>
2024-03-31 21:17:35 +0700

    Implementation of regex matching and rendering in html

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired, Length
 
 class InputForm(FlaskForm):
     regex = StringField('Regex', validators=[DataRequired()])
-    test_string = StringField('Test_string', validators=[DataRequired()])
+    test_string = TextAreaField('Test_regex', 
+                                validators=[DataRequired(), Length(min=1, max=800)])
     submit = SubmitField('Submit')
 
--- app/routes.py
+++ app/routes.py
@@ -1,25 +1,54 @@
 from app import app
 
 from flask import render_template
-# import re
+from app.forms import InputForm
+import re
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=(['GET', 'POST']))
 def index():
-    return render_template('index.html')
-
-# def process_inputs():
-#     if request.method == "POST":
-#         regex = request.form["test-regex"]
-#         test_string = request.form['test-string']
-
-#         found_matches = find_matches(regex, test_string)
+    form = InputForm()
+    if form.validate_on_submit():
+        regex = form.regex.data
+        test_string = form.test_string.data
+        matches_as_list = re.findall(regex, test_string)
+        matches_iter = re.finditer(regex, test_string)
+        num_matches = len(matches_as_list)
+        if not matches_as_list:
+            match_result = 'No matches.'
+        else:
+            match_result = highlight_matches(matches_iter, test_string)
+        match_list = list_of_matches(regex, test_string)
+        captures = match_captures(regex, test_string)
         
-#         return render_template('index.html', 
-#                                found_matches=found_matches)
+        return render_template('index.html', form=form, 
+                               match_result=match_result,
+                               num_matches=num_matches,
+                               match_list=match_list,
+                               captures=captures)
     
-#     else:
-#         return render_template('index.html', output="")
-        
-# def find_matches(regex, test_string):
-#     return re.findall(regex, test_string)
+    return render_template('index.html', form=form)
+
+
+def highlight_matches(matches, string):
+    matches = [m.group() for m in matches] # get list of matches out of iterator
+    matches = set(matches) # removing duplicates
+    for match in matches:
+        regex = f"{match}"
+        highlighted_matches = re.sub(regex, lambda x: "<span>"+x.group()+"</span>", string)
+        string = highlighted_matches
+
+    return highlighted_matches
+
+
+def list_of_matches(regex, string):
+    matches = re.finditer(regex, string)
+    match_list = [(m.span(), m.group()) for m in matches]
+
+    return match_list
+
+
+def match_captures(regex, string):
+    matches = re.finditer(regex, string)
+    captures = [(m.groups(), num + 1) for num, m in enumerate(matches)]
+    
+    return captures
